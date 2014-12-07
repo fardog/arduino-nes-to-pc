73d860254bdb668e13c4a01b279aa76bb6130994
fardog <nate@milkandtang.com>
2014-12-06 21:57:39 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,35 @@
+# Arduino NES/SNES Adapter
+
+An Arduino sketch that exposes an NES (and later, SNES) controller(s) to a
+connected PC. This is accomplished simply: some newer Arduino boards (such as
+the [Leonardo][leonardo] and [Due][due]) can emulate a USB Keyboard/Mouse. This
+sketch just maps the controller buttons to keypresses.
+
+[due]: http://arduino.cc/en/Main/ArduinoBoardDue
+[leonardo]: http://arduino.cc/en/Guide/ArduinoLeonardoMicro?from=Guide.ArduinoLeonardo
+
+## Usage
+
+Instructions, wiring diagrams, and other info will be provided once this is a
+little further along!
+
+## The MIT License (MIT)
+
+Copyright (c) 2014 Nathan Wittstock
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ nescontroller.ino
@@ -0,0 +1,86 @@
+/*
+NES to Keyboard Controller
+By Nathan Wittstock <nate@milkandtang.com>
+Adapted from a sketch by Sebastian Tomczak
+2014-12-06
+MIT Licensed
+*/
+
+int CLOCK = 22; // set the CLOCK pin
+int LATCH = 24; // set the LATCH pin
+int DATA = 26;// set the data in pin
+byte last_controller_data = 0;
+byte controller_data = 0;
+
+/* Controller Keys */
+byte KEYS[] = {
+  /* Controller 1 */
+  0xD7, // Right, KEY_RIGHT_ARROW
+  0xD8, // Left, KEY_LEFT_ARROW
+  0xD9, // Down, KEY_DOWN_ARROW
+  0xDA, // Up, KEY_UP_ARROW
+  0x5D, // Start, ']'
+  0x5B, // Select, '['
+  0x7A, // B, 'z'
+  0x78 // A, 'x'
+};
+
+bool keys_state[] = {1, 1, 1, 1, 1, 1, 1, 1};
+
+/* SETUP */
+void setup() {
+  Serial.begin(57600);
+  Keyboard.begin();
+  
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(DATA, INPUT);
+
+  digitalWrite(LATCH, HIGH);
+  digitalWrite(CLOCK, HIGH);
+}
+
+void controllerRead() {
+  controller_data = 0;
+  digitalWrite(LATCH, LOW);
+  digitalWrite(CLOCK, LOW);
+
+  digitalWrite(LATCH, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(LATCH, LOW);
+
+  controller_data = digitalRead(DATA);
+
+  for (int i = 1; i < 8; i++) {
+    digitalWrite(CLOCK, HIGH);
+    delayMicroseconds(2);
+    controller_data = controller_data << 1;
+    controller_data = controller_data + digitalRead(DATA) ;
+    delayMicroseconds(4);
+    digitalWrite(CLOCK, LOW);
+  }
+}
+
+void loop() {
+  controllerRead();
+  if (controller_data != last_controller_data) {
+    // update our keys
+    for (int i = 0; i < 8; i++) {
+      byte controller_value = controller_data & (1 << i);
+      byte last_value = last_controller_data & (1 << i);
+      
+      if (controller_value != last_value) {
+        if (keys_state[i] == 0) {
+          Keyboard.press(KEYS[i]);
+          keys_state[i] = 1;
+        }
+        else {
+          Keyboard.release(KEYS[i]);
+          keys_state[i] = 0;
+        }
+      }
+    }
+  }
+  last_controller_data = controller_data;
+  delay(10);
+}
